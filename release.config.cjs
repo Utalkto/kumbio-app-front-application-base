705eb5edbde8c527fc3fc45b512a32cca569dc94
Carlos <correamillancarlos@gmail.com>
2023-11-11 18:01:52 -0400

    fix: change release config

--- release.config.cjs
+++ release.config.cjs
@@ -3,15 +3,21 @@ const config = {
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
-		[
-			'@semantic-release/git',
-			{
-				assets: ['dist/*.js', 'dist/*.js.map'],
-				message:
-					'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-			},
-		],
+		'@semantic-release/changelog',
 		'@semantic-release/github',
+		'@semantic-release/git',
+		// '@semantic-release/commit-analyzer',
+		// '@semantic-release/release-notes-generator',
+		// '@semantic-release/changelog',
+		// [
+		// 	'@semantic-release/git',
+		// 	{
+		// 		assets: ['.next/*.js', '.next/*.js.map'],
+		// 		message:
+		// 			'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+		// 	},
+		// ],
+		// '@semantic-release/github',
 	],
 };
 
