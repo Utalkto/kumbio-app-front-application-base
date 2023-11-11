95dae5a7c7d2ce1550ccc397f7f7280bdfa8a662
Carlos <correamillancarlos@gmail.com>
2023-11-11 16:56:22 -0400

    feat: add p tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
 	return (
 		<main>
 			<p>Hi Kumbio</p>
+			<p>Hi Kumbio2</p>
 			<Button variant="contained">BTN Kumbio</Button>
 			<Button variant="contained">BTN Kumbio</Button>
 		</main>
