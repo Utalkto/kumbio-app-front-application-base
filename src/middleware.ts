36895a55002e03b8d11da96075d35cd6b97acf07
Carlos <correamillancarlos@gmail.com>
2023-11-25 13:41:44 -0400

    feat: settings vscode and configuration middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,20 @@
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
-import { IsRoute } from './routes';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import { onIsProtectedRoute } from './routes';
 
-export async function middleware (request: NextRequest) {
+export async function middleware(request: NextRequest) {
+	const isToken = request.cookies.get('token') ? true : false;
+	const { pathname } = request.nextUrl;
 
-    const isToken = request.cookies.get('token') ? true : false;
-    const { pathname } = request.nextUrl
+	const isProtectedRoute = onIsProtectedRoute(pathname);
 
-    const currentRoute = IsRoute(pathname);
+	if (isProtectedRoute && !isToken) {
+		return NextResponse.redirect(new URL('/login', request.url));
+	}
 
-    if( currentRoute ) {
-        
-        if( currentRoute.protected && currentRoute.protected && !isToken){
-            return NextResponse.redirect(new URL('/', request.url));
-        }
-
-    }
-
-    return NextResponse.next()
-  
+	return NextResponse.next();
 }
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|.png).*)'],
-}
+	matcher: ['/((?!api|_next/static|_next/image|.png).*)'],
+};
--- src/providers.tsx
+++ src/providers.tsx
@@ -4,11 +4,9 @@ import { ProviderStore } from './store/ProviderStore';
 import RootProviders from '@/context/RootProvider';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return (
-        <ProviderStore>
-            <RootProviders>
-                { children }
-            </RootProviders>
-        </ProviderStore>
-    )
+	return (
+		<ProviderStore>
+			<RootProviders>{children}</RootProviders>
+		</ProviderStore>
+	);
 }
