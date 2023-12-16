a88aa35922ae84070312b3812232f4dd18ea9584
Carlos <correamillancarlos@gmail.com>
2023-12-16 10:23:05 -0400

    wip

--- src/layouts/MobileLayout/index.tsx
+++ src/layouts/MobileLayout/index.tsx
@@ -3,9 +3,7 @@
 import React from 'react';
 import { Props } from './interfaces';
 import { FC } from 'react';
-
 import { Navbar } from './components';
-import { Stack } from '@mui/material';
 
 export const MobileLayout: FC<Props> = ({ children }) => {
 	return (
