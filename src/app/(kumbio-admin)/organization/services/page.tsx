3b98d1c94ce8d638e272a3dd97f89da330a2613f
Carlos <correamillancarlos@gmail.com>
2023-12-26 21:56:15 -0400

    fix: hide service page

--- src/app/(kumbio-admin)/organization/services/page.tsx
+++ src/app/(kumbio-admin)/organization/services/page.tsx
@@ -1,13 +1,8 @@
-import { HeroService } from '@/components';
 import { Stack } from '@mui/material';
 import React from 'react';
 
 const ServicesPage = () => {
-	return (
-		<Stack width={'100%'} gap={2}>
-			<HeroService />
-		</Stack>
-	);
+	return <Stack width={'100%'} gap={2}></Stack>;
 };
 
 export default ServicesPage;
